10ab6663196676a4d779253f183a88201c8c8370
Ariel Maniago <admaniago01@gmail.com>
2024-01-26 23:43:30 +0800

    adding content on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex gap-10'>
-      <Link className="navlinks text-sm" to="/">Home</Link>
-      <Link className="navlinks text-sm" to="journal">Journal</Link>
-      <Link className="navlinks text-sm" to="gallery">Gallery</Link>
-      <Link className="navlinks text-sm" to="about">About</Link>
+    <div className='flex flex-col h-[50vh] justify-around'>
+      <div>
+        <Link className="navlinks text-sm" to="/">Home</Link>
+      </div>
+      <div>
+        <Link className="navlinks text-sm" to="journal">Journal</Link>
+      </div>
+      <div>
+        <Link className="navlinks text-sm" to="gallery">Gallery</Link>
+      </div>
+      <div>
+        <Link className="navlinks text-sm" to="about">About</Link>
+      </div>
     </div>
   )
 }
