de899b2774dc5c35380c62033a6903720a4c7a74
Ariel Maniago <admaniago01@gmail.com>
2024-01-28 22:40:04 +0800

    adding some changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,13 +7,13 @@ import { blurhashToCssGradientString } from '@unpic/placeholder';
 const Home = () => {
   const blurPlaceholder = blurhashToCssGradientString('LoGbbZE3kBae~pIpayjbx]V@affl')
   return (
-    <div>
+    <>
       <section>
-        <div className='flex py-10 flex-col md:flex-col lg:flex-row gap-4'>
+        <div className='flex my-10 flex-col md:flex-row lg:flex-row gap-4'>
           <div className='flex items-center'>
             <div>
               <h1 className='text-3xl'>"Unraveling the layers of my thoughts, dreams, and the raw essence of my existence"</h1>
-              <div className='mt-4 flex justify-start md:justify-center lg:justify-start'>
+              <div className='mt-4 flex justify-start md:justify-start lg:justify-start'>
                 <Button variant='outline' className='bg-transparent border-2 border-[#97978D] rounded-full px-10' asChild>
                   <Link to='/about'>About</Link>
                 </Button>
@@ -29,7 +29,7 @@ const Home = () => {
       <section>
         <div className='py-10'>
           <h2 className='text-2xl mb-4'>Recent Journal</h2>
-          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
             <div className="h-32 rounded-lg bg-gray-200 lg:col-span-2"></div>
             <div className="h-32 rounded-lg bg-gray-200"></div>
             <div className="h-32 rounded-lg bg-gray-200"></div>
@@ -47,7 +47,7 @@ const Home = () => {
           <h2 className='text-2xl'>Recent Uploads</h2>
         </div>
       </section>
-    </div>
+    </>
   )
 }
 
