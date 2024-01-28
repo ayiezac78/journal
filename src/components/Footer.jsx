98b67ef34935db946f77a85d72123b7eb2887fb0
Ariel Maniago <admaniago01@gmail.com>
2024-01-28 13:39:55 +0800

    added content in homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import InlineSVG from 'react-inlinesvg'
-import logo from '../assets/images/svg/logo-no-background.svg'
+import logo from '../assets/images/svg/footer-logo.svg'
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-100'>
-      <div class="mx-auto px-4 py-8 sm:px-6 lg:px-10 container">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <div class="flex justify-center text-teal-600 sm:justify-start">
+    <footer className='bg-[#97978D]'>
+      <div className="mx-auto px-4 py-8 sm:px-6 lg:px-10 container">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="flex justify-center text-teal-600 sm:justify-start">
             {/* <span className='logo'>journallery</span> */}
             <InlineSVG src={logo} />
           </div>
 
-          <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          <p className="mt-4 text-center text-sm text-slate-50 lg:mt-0 lg:text-right">
             Copyright &copy; {new Date().getFullYear()} . All rights reserved.
           </p>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Cloudinary } from '@cloudinary/url-gen'
+import { AdvancedImage, lazyload, responsive, placeholder, accessibility } from '@cloudinary/react';
+import { scale } from "@cloudinary/url-gen/actions/resize";
+import { Separator } from '@/components/ui/separator';
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 const Home = () => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dkar2rexr'
+    }
+  })
+
+  const orangeSunset = cld.image('images/memories/bike-on-a-sunset');
+  orangeSunset.resize(scale().width(600));
+  orangeSunset.format('auto').quality('auto')
+
   return (
     <div>
-      <h1 className='py-10 text-4xl'>Home</h1>
+      <section>
+        <div className='flex py-10 flex-col md:flex-col lg:flex-row gap-4'>
+          <div className='flex items-center'>
+            <div>
+              <h1 className='text-3xl'>"Unraveling the layers of my thoughts, dreams, and the raw essence of my existence."</h1>
+              <div className='mt-4 flex justify-start md:justify-center lg:justify-start'>
+                <Button variant='outline' className='bg-transparent border-2 border-[#97978D] rounded-full px-10' asChild>
+                  <Link to='/about'>About</Link>
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className='w-fit md:w-full lg:w-full flex justify-center'>
+            <AdvancedImage
+              className=' rounded-md grayscale hover:grayscale-0 transition-all'
+              cldImg={orangeSunset}
+              plugins={
+                [
+                  lazyload(),
+                  responsive(),
+                  placeholder({ mode: 'predominant' })
+                ]
+              }
+            ></AdvancedImage>
+          </div>
+        </div>
+      </section>
+      <Separator />
+      <section>
+        <div className='py-10'>
+          <h2 className='text-2xl mb-4'>Recent Journal</h2>
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+            <div className="h-32 rounded-lg bg-gray-200 lg:col-span-2"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+          </div>
+          <div className='mt-4 flex justify-center md:justify-center lg:justify-start'>
+            <Button variant='outline' className='bg-transparent border-2 border-[#97978D] rounded-full px-10' asChild>
+              <Link to='/journal'>See More</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
